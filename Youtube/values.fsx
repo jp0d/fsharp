237211e23ab49b411243d22cefbecf28fd772042
Jajati Sahoo <jajati.sahoo@xero.com>
2021-08-04 23:09:16 +1000

    Create values.fsx

---
+++ Youtube/values.fsx
@@ -0,0 +1,49 @@
+
+(* Value types *)
+
+// String value
+"New York"
+
+// Boolean Value
+true
+
+// Tuple
+(1.0, 3.0)
+
+// List Value
+["Learn F#"; "Build App"; "Implement Algorithm!"]
+
+// Lambda expression / Anonymous function value
+
+fun input -> input / 3
+
+
+(* Value bindings *)
+let cityName = "New York"
+
+let fSharpIsAwesome = true
+
+let coordinates = (1.0, 3.0) 
+
+let toDoList = ["Learn F#"; "Build App"; "Implement Algorithm!"]
+
+let divideByThree input = input / 3
+
+(* Explicit type annotations *)
+let (luckyNumberString:string) = "13"
+
+(* Updating values *)
+
+// cityName <- "New York"
+
+let newCityName = "Melbourne"
+
+(* Mutable values *)
+let mutable mutableCityName = "New York"
+
+mutableCityName <- "Melbourne"
+
+
+
+
+
