e1c7e61ea8847bd97b3498676ac972df473ebeda
Jajati Sahoo <jajati.sahoo@xero.com>
2021-08-08 19:23:41 +1000

    Create functions.fsx

---
+++ Youtube/functions.fsx
@@ -0,0 +1,22 @@
+
+(** Lambda expressions **)
+// Lambda expressions are defined using fun keywork
+// Can have no parameters or multiple parameters
+
+fun () -> printfn "Hello F#"
+fun x -> x + 1
+fun x y -> x + y
+
+
+(** Defining named function **)
+
+let addOne x = x + 1 
+
+(** Calling functions **)
+addOne 3
+
+
+
+
+
+
